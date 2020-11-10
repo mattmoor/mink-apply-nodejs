ac5435e87d94a39426c3e80a755efef118d944cd
Matt Moore <mattmoor@vmware.com>
2020-11-09 21:02:11 -0800

    Update comments in /index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// Trying to sort out why the GCP buildpack doesn't honor
-// the (cloudevent) => (cloudevent) signature.
+// With GOOGLE_FUNCTION_SIGNATURE_TYPE=cloudevent the GCP function
+// framework does not properly serialize the response CloudEvent,
+// so it is useless.  Instead use the HTTP signature and wrap things
+// ourselves using ToHTTP.
 const ToHTTP = require('./http').ToHTTP
 
 // Ideally the GCP buildpack would support GOOGLE_FUNCTION_SOURCE
-// and I think we could dump this file. 
+// and I think we could dump this file (once above is addressed).
 exports.divide = ToHTTP(require('./divide').event)
 exports.swap = ToHTTP(require('./swap').event)
 exports.random = ToHTTP(require('./random').event)
